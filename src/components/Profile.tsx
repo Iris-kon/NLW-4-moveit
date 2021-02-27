4ab79244823f8a7670f414493157c228a11c6843
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2021-02-27 20:47:13 -0300

    add challege context and not stactic xp

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../context/ChallengesContext'
+
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://pbs.twimg.com/profile_images/1327332074521571336/rKgvfzg4_400x400.jpg" alt=""/>
@@ -8,7 +13,7 @@ export function Profile() {
                 <strong>Thiago Oliveira</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Lvl 1
+                    Level {level}
                 </p>
             </div>
         </div>
