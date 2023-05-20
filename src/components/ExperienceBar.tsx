fb1dab49aa0bdab41b8a0e93c6721daf4598747e
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2023-05-20 02:39:00 -0300

    update dependencies and adjust experience bar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -4,19 +4,26 @@ import { ChallengesContext } from '../context/ChallengesContext'
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
-    const { currentExperience, expirienceToNextLevel } = useContext(ChallengesContext)
+  const { currentExperience, expirienceToNextLevel } =
+    useContext(ChallengesContext)
 
-    const percentToNextLevel = Math.round((currentExperience * 100) / expirienceToNextLevel)
+  const percentToNextLevel =
+    Math.round((currentExperience * 100) / expirienceToNextLevel) ?? 0
 
-    return (
-        <header className={styles.experienceBar}>
-            <span>0 xp</span>
-            <div>
-                <div style={{ width: `${percentToNextLevel}%` }} />
-            
-                <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>{currentExperience} xp</span>
-            </div>
-            <span>{expirienceToNextLevel} xp</span>
-        </header>
-    )
+  return (
+    <header className={styles.experienceBar}>
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: `${percentToNextLevel}%` }} />
+
+        <span
+          className={styles.currentExperience}
+          style={{ left: `${percentToNextLevel}%` }}
+        >
+          {currentExperience} xp
+        </span>
+      </div>
+      <span>{expirienceToNextLevel} xp</span>
+    </header>
+  )
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,18 +4,21 @@ import { ChallengesContext } from '../context/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
-    const { level } = useContext(ChallengesContext)
+  const { level } = useContext(ChallengesContext)
 
-    return (
-        <div className={styles.profileContainer}>
-            <img src="https://pbs.twimg.com/profile_images/1327332074521571336/rKgvfzg4_400x400.jpg" alt=""/>
-            <div>
-                <strong>Thiago Oliveira</strong>
-                <p>
-                    <img src="icons/level.svg" alt="Level"/>
-                    Level {level}
-                </p>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://avatars.cloudflare.steamstatic.com/ec310121643cdcf57186478d7368c9b59c0aec5b_full.jpg"
+        alt="profile"
+      />
+      <div>
+        <strong>Íris Oliveira</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  )
 }
